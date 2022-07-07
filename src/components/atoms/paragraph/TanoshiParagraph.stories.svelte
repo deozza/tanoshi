2ddab965e5f83987e1ad958d10b8de796336eab0
deozza <deozza@gmail.com>
2022-07-08 00:39:35 +0200

    typo + added comments to show object construction

--- src/components/atoms/paragraph/TanoshiParagraph.stories.svelte
+++ src/components/atoms/paragraph/TanoshiParagraph.stories.svelte
@@ -3,12 +3,12 @@
 	import TanoshiParagraphModel from './TanoshiParagraphModel';
 	import TanoshiParagraph from './TanoshiParagraph.svelte';
 
-	const h1Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setdisplaySize('3xl')
-	const h2Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setdisplaySize('2xl')
-	const h3Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setdisplaySize('xl')
-	const h4Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setdisplaySize('lg')
-	const h5Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setdisplaySize('base')
-	const h6Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setdisplaySize('sm')
+	const h1Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setDisplaySize('3xl')
+	const h2Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setDisplaySize('2xl')
+	const h3Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setDisplaySize('xl')
+	const h4Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setDisplaySize('lg')
+	const h5Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setDisplaySize('base')
+	const h6Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setDisplaySize('sm')
 
 	const blackParagraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet")
 	const whiteParagraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setTheme('white')
@@ -17,6 +17,8 @@
 	const successParagraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setTheme('success')
 	const warningParagraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setTheme('warning')
 	const dangerParagraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setTheme('danger')
+
+	const styledParagraph = new TanoshiParagraphModel("<span class='uppercase'>Lorem</span> <span class='underline'>ipsum</span> <span class='italic'>dolor</span> <span class='font-bold'>sit</span> <span class='line-through'>amet</span>")
 
 </script>
 
@@ -55,26 +57,56 @@
 />
 <Story name="Paragraphs sizes">
 	<div>
+		<!-- 	const h1Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setDisplaySize('3xl')-->
 		<TanoshiParagraph tanoshiParagraphModel='{h1Paragraph}'  />
+
+		<!-- 	const h2Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setDisplaySize('2xl') -->
 		<TanoshiParagraph tanoshiParagraphModel='{h2Paragraph}'  />
+
+		<!-- 	const h3Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setDisplaySize('xl') -->
 		<TanoshiParagraph tanoshiParagraphModel='{h3Paragraph}'  />
+
+		<!-- 	const h4Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setDisplaySize('lg') -->
 		<TanoshiParagraph tanoshiParagraphModel='{h4Paragraph}'  />
+
+		<!-- 	const h5Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setDisplaySize('base') -->
 		<TanoshiParagraph tanoshiParagraphModel='{h5Paragraph}'  />
+
+		<!-- 	const h6Paragraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setDisplaySize('sm') -->
 		<TanoshiParagraph tanoshiParagraphModel='{h6Paragraph}'  />
 	</div>
 </Story>
 
 <Story name="Colored Paragraphs">
 	<div>
+		<!-- 	const blackParagraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet") -->
 		<TanoshiParagraph tanoshiParagraphModel='{blackParagraph}'  />
+
 		<div class='bg-black'>
+			<!-- 	const whiteParagraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setTheme('white') -->
 			<TanoshiParagraph tanoshiParagraphModel='{whiteParagraph}'  />
 		</div>
+
+		<!-- 	const secondaryParagraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setTheme('secondary') -->
 		<TanoshiParagraph tanoshiParagraphModel='{secondaryParagraph}'  />
+
+		<!-- 	const infoParagraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setTheme('info') -->
 		<TanoshiParagraph tanoshiParagraphModel='{infoParagraph}'  />
+
+		<!-- 	const successParagraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setTheme('success') -->
 		<TanoshiParagraph tanoshiParagraphModel='{successParagraph}'  />
+
+		<!-- 	const warningParagraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setTheme('warning') -->
 		<TanoshiParagraph tanoshiParagraphModel='{warningParagraph}'  />
+
+		<!-- 	const dangerParagraph = new TanoshiParagraphModel("Lorem ipsum dolor sit amet").setTheme('danger') -->
 		<TanoshiParagraph tanoshiParagraphModel='{dangerParagraph}'  />
 	</div>
 </Story>
 
+<Story name="Styled Paragraphs">
+	<div>
+		<!-- const styledParagraph = new TanoshiParagraphModel("<span class='uppercase'>Lorem</span> <span class='underline'>ipsum</span> <span class='italic'>dolor</span> <span class='font-bold'>sit</span> <span class='line-through'>amet</span>") -->
+		<TanoshiParagraph tanoshiParagraphModel='{styledParagraph}'  />
+	</div>
+</Story>
