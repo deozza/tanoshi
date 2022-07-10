6eed9a8c50a8cc42c42ae0531168ef82d8bcde11
Edenn Touitou <deozza@gmail.com>
2022-07-10 14:06:07 +0000

    basic input model

---
+++ src/components/atoms/input/TanoshitInputModel.ts
@@ -0,0 +1,93 @@
+export default class TanoshiInputModel {
+    protected _type: string;
+	protected _id: string;
+	protected _name: string;
+	protected _required: boolean;
+	protected _value: string | null;
+	protected _readonly: boolean;
+	protected _error: boolean;
+	protected _errorMessage: string;
+
+	public constructor(name: string) {
+        this._type = '';
+		this._name = name;
+		this._id = name;
+		this._required = false;
+		this._value = null;
+		this._readonly = false;
+		this._error = false;
+		this._errorMessage = '';
+	}
+
+    get type(): string {
+		return this._type;
+	}
+
+	setType(value: string): TanoshiInputModel {
+		this._type = value;
+        return this;
+	}
+
+	get id(): string {
+		return this._id;
+	}
+
+	setId(value: string): TanoshiInputModel {
+		this._id = value;
+        return this;
+	}
+
+	get name(): string {
+		return this._name;
+	}
+
+	setName(value: string): TanoshiInputModel {
+		this._name = value;
+        return this;
+	}
+
+	get required(): boolean {
+		return this._required;
+	}
+
+	setRequired(value: boolean): TanoshiInputModel {
+		this._required = value;
+        return this;
+	}
+
+	get value(): string | null {
+		return this._value;
+	}
+
+	setValue(value: string | null): TanoshiInputModel {
+		this._value = value;
+        return this;
+	}
+
+	get readonly(): boolean {
+		return this._readonly;
+	}
+
+	setReadonly(value: boolean): TanoshiInputModel {
+		this._readonly = value;
+        return this;
+	}
+
+	get error(): boolean {
+		return this._error;
+	}
+
+	setError(value: boolean): TanoshiInputModel {
+		this._error = value;
+        return this;
+	}
+
+	get errorMessage(): string {
+		return this._errorMessage;
+	}
+
+	setErrorMessage(value: string): TanoshiInputModel {
+		this._errorMessage = value;
+        return this;
+	}
+}
