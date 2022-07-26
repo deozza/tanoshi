04bc41089f799a0482b0ba787813429b11c1e793
Edenn Touitou <deozza@gmail.com>
2022-07-26 20:21:35 +0000

    renaming and better style

--- src/components/molecules/navigation/TanoshiNavigation.svelte
+++ src/components/molecules/navigation/TanoshiNavigation.svelte
@@ -14,14 +14,13 @@
     import TanoshiButtonModel from "../../atoms/button/TanoshiButtonModel";
 
 	export let tanoshiNavigationModel: TanoshiNavigationModel
+
 	const horizontalNavigationContainer = new TanoshiContainerModel('r').setTheme(tanoshiNavigationModel.theme).setDesktopSpacing('start')
-    
-	const desktopNavigationContainer = new TanoshiContainerModel('r').setTheme(tanoshiNavigationModel.theme).setDesktopSpacing('start')
+    const mobileNavigationContainer = new TanoshiContainerModel('c').setTheme(tanoshiNavigationModel.theme)
+    const desktopNavigationContainer = new TanoshiContainerModel('r').setTheme(tanoshiNavigationModel.theme).setDesktopSpacing('start')
     if(tanoshiNavigationModel.orientation === 'vertical') {
         desktopNavigationContainer.setDesktopOrientation('c')
     }
-
-    const navigationMobileContainer = new TanoshiContainerModel('c').setTheme(tanoshiNavigationModel.theme)
 
     const mobileMenuButton: TanoshiButtonModel = new TanoshiButtonModel("").setTheme(tanoshiNavigationModel.theme)
 
@@ -39,12 +38,10 @@
 		showMobileMenu = !showMobileMenu;
 	}
 
-
 </script>
 
 
-
-<nav class="hidden sm:block">
+<nav class="fixed hidden sm:block">
     <TanoshiContainer tanoshiContainerModel={desktopNavigationContainer} customClasses="{tanoshiNavigationModel.orientation === "horizontal" ? "h-16" : "min-h-screen w-1/5"}">
         {#each tanoshiNavigationModel.items as tanoshiLinkModel}
             <TanoshiLink  tanoshiLinkModel="{tanoshiLinkModel}" />
@@ -52,7 +49,7 @@
     </TanoshiContainer>
 </nav>
 
-<nav class="sm:hidden">
+<nav class="fixed sm:hidden">
     <TanoshiContainer tanoshiContainerModel={horizontalNavigationContainer} customClasses="h-16">
         <TanoshiButton tanoshiButtonModel="{mobileMenuButton}"  on:click={mobileMenuHandler}>
             <span class="sr-only">Open main menu</span>
@@ -66,7 +63,7 @@
 
     {#if showMobileMenu === true}
         <div id="mobile-menu">
-            <TanoshiContainer tanoshiContainerModel={navigationMobileContainer}>
+            <TanoshiContainer tanoshiContainerModel={mobileNavigationContainer}>
                 {#each tanoshiNavigationModel.items as tanoshiLinkModel}
                     <TanoshiLink  tanoshiLinkModel="{tanoshiLinkModel}" />
                 {/each}
@@ -75,45 +72,3 @@
     {/if}
 
 </nav>
-<!--
-
-<nav class="fixed w-full bg-sky-800">
-	<div class="px-2 sm:px-6 lg:px-8">
-		<div class="relative flex items-center justify-between h-16">
-			<div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-				<button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false" on:click={mobileMenuHandler}>
-					<span class="sr-only">Open main menu</span>
-                	{#if showMobileMenu === false}
-                        <Fa icon={faBars} />
-                    {:else}
-                        <Fa icon={faTimes} />
-                    {/if}
-
-				</button>
-			</div>
-			<div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-				<div class="flex-shrink-0 flex items-center">
-					<a  href='/' class='text-white font-bold text-2xl'>Appquarium</a>
-				</div>
-				<div class="w-full hidden sm:block sm:ml-6">
-					<div class="w-full justify-center flex space-x-4">
-                        {#each tanoshiNavigationModel.items as tanoshiLinkModel}
-                            <TanoshiLink  tanoshiLinkModel="{tanoshiLinkModel}" />
-						{/each}
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	{#if showMobileMenu === true}
-		<div class="sm:hidden" id="mobile-menu">
-            <TanoshiContainer tanoshiContainerModel={navigationMobileContainer}>
-				{#each tanoshiNavigationModel.items as tanoshiLinkModel}
-                    <TanoshiLink  tanoshiLinkModel="{tanoshiLinkModel}" />
-				{/each}
-			</TanoshiContainer>
-		</div>
-	{/if}
-</nav>
--->
