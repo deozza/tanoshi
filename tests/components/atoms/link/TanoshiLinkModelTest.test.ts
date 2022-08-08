03f1787bad31cc1fab431e894dee99c7187e61d4
Edenn Touitou <deozza@gmail.com>
2022-08-08 21:43:56 +0000

    link model is tested

---
+++ tests/components/atoms/link/TanoshiLinkModelTest.test.ts
@@ -0,0 +1,27 @@
+import TanoshiLinkModel from '../../../../src/components/atoms/link/TanoshiLinkModel'
+
+test('default values are valid',  () => {
+
+	const defaultLink: TanoshiLinkModel = new TanoshiLinkModel('')
+
+    expect(defaultLink.content).toEqual('')
+    expect(defaultLink.link).toEqual('#')
+    expect(defaultLink.isInternal).toEqual('_self')
+})
+
+test('value in constructor affects content',  () => {
+
+	const defaultLink: TanoshiLinkModel = new TanoshiLinkModel('this is a content')
+
+    expect(defaultLink.content).toEqual('this is a content')
+})
+
+test('boolean for isInternal translates to html value', () => {
+	const defaultLink: TanoshiLinkModel = new TanoshiLinkModel('')
+    
+    defaultLink.setIsInternal(true)
+    expect(defaultLink.isInternal).toEqual('_self')
+
+    defaultLink.setIsInternal(false)
+    expect(defaultLink.isInternal).toEqual('_blank');
+})
