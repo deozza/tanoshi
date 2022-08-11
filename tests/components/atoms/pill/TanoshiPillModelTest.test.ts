6afd3d83b9e8d4791216a71097d17e6d392b37ec
Edenn Touitou <deozza@gmail.com>
2022-08-11 00:42:25 +0000

    pill model is tested

---
+++ tests/components/atoms/pill/TanoshiPillModelTest.test.ts
@@ -0,0 +1,32 @@
+import TanoshiPillModel from '$lib/atoms/pill/TanoshiPillModel';
+
+test('default values are valid', () => {
+	const defaultPill: TanoshiPillModel = new TanoshiPillModel('');
+
+	expect(defaultPill.content).toEqual('');
+	expect(defaultPill.size).toEqual('md');
+	expect(defaultPill.theme).toEqual('primary');
+	expect(defaultPill.isOutlined).toEqual(false);
+});
+
+test('value in constructor affects content', () => {
+	const defaultPill: TanoshiPillModel = new TanoshiPillModel('this is a content');
+
+	expect(defaultPill.content).toEqual('this is a content');
+});
+
+test('expect Exception when displaySize is invalid', () => {
+	const defaultPill: TanoshiPillModel = new TanoshiPillModel('');
+
+	expect(() => {
+		defaultPill.setSize('invalid');
+	}).toThrow();
+});
+
+test('expect Exception when theme is invalid', () => {
+	const defaultPill: TanoshiPillModel = new TanoshiPillModel('');
+
+	expect(() => {
+		defaultPill.setTheme('invalid');
+	}).toThrow();
+});
