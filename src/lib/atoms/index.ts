f6855a38c9b0190b77466d3fe5a946fac7489a6f
Edenn Touitou <deozza@gmail.com>
2023-05-11 15:50:57 +0000

    added peerDep, index files to export components

---
+++ src/lib/atoms/index.ts
@@ -0,0 +1,24 @@
+export {default as TanoshiButton} from "./button/TanoshiButton.svelte";
+export {default as TanoshiButtonModel} from "./button/TanoshiButtonModel";
+
+export {default as TanoshiHeader} from "./header/TanoshiHeader.svelte";
+export {default as TanoshiHeaderModel} from "./header/TanoshiHeaderModel";
+
+export {default as TanoshiImage} from "./image/TanoshiImage.svelte";
+export {default as TanoshiImageInCarousel} from "./image/TanoshiImageInCarousel.svelte";
+export {default as TanoshiImageModel} from "./image/TanoshiImageModel";
+
+export {default as TanoshiTextInput} from "./input/text/TanoshiTextInput.svelte";
+export {default as TanoshiTextInputModel} from "./input/text/TanoshiTextInputModel";
+
+export {default as TanoshiLabel} from "./label/TanoshiLabel.svelte";
+export {default as TanoshiLabelModel} from "./label/TanoshiLabelModel";
+
+export {default as TanoshiLink} from "./link/TanoshiLink.svelte";
+export {default as TanoshiLinkModel} from "./link/TanoshiLinkModel";
+
+export {default as TanoshiParagraph} from "./paragraph/TanoshiParagraph.svelte";
+export {default as TanoshiParagraphModel} from "./paragraph/TanoshiParagraphModel";
+
+export {default as TanoshiPill} from "./pill/TanoshiPill.svelte";
+export {default as TanoshiPillModel} from "./pill/TanoshiPillModel";
---
+++ src/lib/molecules/index.ts
@@ -0,0 +1,14 @@
+export {default as TanoshiAlert} from "./alert/TanoshiAlert.svelte";
+export {default as TanoshiAlertModel} from "./alert/TanoshiAlertModel";
+
+export {default as TanoshiCarousel} from "./carousel/TanoshiCarousel.svelte";
+export {default as TanoshiCarouselModel} from "./carousel/TanoshiCarouselModel";
+
+export {default as TanoshiContainer} from "./container/TanoshiContainer.svelte";
+export {default as TanoshiContainerModel} from "./container/TanoshiContainerModel";
+
+export {default as TanoshiForm} from "./form/TanoshiForm.svelte";
+export {default as TanoshiFormModel} from "./form/TanoshiFormModel";
+
+export {default as TanoshiNavigation} from "./navigation/TanoshiNavigation.svelte";
+export {default as TanoshiNavigationModel} from "./navigation/TanoshiNavigationModel";
