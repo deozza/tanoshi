c53882735d1df0ee808f49713b0c44f5c4ae36af
Edenn Touitou <deozza@gmail.com>
2023-09-15 16:04:26 +0000

    added exports

--- src/lib/atoms/index.ts
+++ src/lib/atoms/index.ts
@@ -1,15 +1,27 @@
 export {default as TanoshiButton} from "./button/TanoshiButton.svelte";
 export {default as TanoshiButtonModel} from "./button/TanoshiButtonModel";
 
-export {default as TanoshiHeader} from "./typography/header/TanoshiHeader.svelte";
-export {default as TanoshiHeaderModel} from "./typography/header/TanoshiHeaderModel";
-
 export {default as TanoshiImage} from "./image/TanoshiImage.svelte";
 export {default as TanoshiImageInCarousel} from "./image/TanoshiImageInCarousel.svelte";
 export {default as TanoshiImageModel} from "./image/TanoshiImageModel";
 
+export {default as TanoshiChoiceInput} from "./input/choice/TanoshiChoiceInput.svelte";
+export {default as TanoshiChoiceInputModel} from "./input/choice/TanoshiChoiceInputModel";
+
+export {default as TanoshiFileInput} from "./input/file/TanoshiFileInput.svelte";
+export {default as TanoshiFileInputModel} from "./input/file/TanoshiFileInputModel";
+
+export {default as TanoshiNumberInput} from "./input/number/TanoshiNumberInput.svelte";
+export {default as TanoshiNumberInputModel} from "./input/number/TanoshiNumberInputModel";
+
+export {default as TanoshiSelectOption} from "./input/select/option/TanoshiSelectOption.svelte";
+export {default as TanoshiSelectOptionModel} from "./input/select/option/TanoshiSelectOptionModel";
+
 export {default as TanoshiTextInput} from "./input/text/TanoshiTextInput.svelte";
 export {default as TanoshiTextInputModel} from "./input/text/TanoshiTextInputModel";
+
+export {default as TanoshiTextareaInput} from "./input/textarea/TanoshiTextareaInput.svelte";
+export {default as TanoshiTextareaInputModel} from "./input/textarea/TanoshiTextareaInputModel";
 
 export {default as TanoshiLabel} from "./label/TanoshiLabel.svelte";
 export {default as TanoshiLabelModel} from "./label/TanoshiLabelModel";
@@ -17,8 +29,12 @@ export {default as TanoshiLabelModel} from "./label/TanoshiLabelModel";
 export {default as TanoshiLink} from "./link/TanoshiLink.svelte";
 export {default as TanoshiLinkModel} from "./link/TanoshiLinkModel";
 
+export {default as TanoshiPill} from "./pill/TanoshiPill.svelte";
+export {default as TanoshiPillModel} from "./pill/TanoshiPillModel";
+
 export {default as TanoshiParagraph} from "./typography/paragraph/TanoshiParagraph.svelte";
 export {default as TanoshiParagraphModel} from "./typography/paragraph/TanoshiParagraphModel";
 
-export {default as TanoshiPill} from "./pill/TanoshiPill.svelte";
-export {default as TanoshiPillModel} from "./pill/TanoshiPillModel";
+export {default as TanoshiHeader} from "./typography/header/TanoshiHeader.svelte";
+export {default as TanoshiHeaderModel} from "./typography/header/TanoshiHeaderModel";
+
--- src/lib/molecules/index.ts
+++ src/lib/molecules/index.ts
@@ -1,4 +1,5 @@
 export {default as TanoshiAlert} from "./alert/TanoshiAlert.svelte";
+export {default as TanoshiAlertContainer} from "./alert/alertContainer/TanoshiAlertContainer.svelte";
 export {default as TanoshiAlertModel} from "./alert/TanoshiAlertModel";
 
 export {default as TanoshiCarousel} from "./carousel/TanoshiCarousel.svelte";
@@ -10,5 +11,34 @@ export {default as TanoshiContainerModel} from "./container/TanoshiContainerMode
 export {default as TanoshiForm} from "./form/TanoshiForm.svelte";
 export {default as TanoshiFormModel} from "./form/TanoshiFormModel";
 
+export {default as TanoshiChoiceGroup} from "./input/choiceGroup/TanoshiChoiceGroup.svelte";
+export {default as TanoshiChoiceGroupModel} from "./input/choiceGroup/TanoshiChoiceGroupModel";
+
+export {default as TanoshiSelectInput} from "./input/select/TanoshiSelectInput.svelte";
+export {default as TanoshiSelectInputModel} from "./input/select/TanoshiSelectInputModel";
+
+export {default as TanoshiLabelAndInput} from "./labelAndInput/TanoshiLabelAndInput.svelte";
+export {default as TanoshiLabelAndInputModel} from "./labelAndInput/TanoshiLabelAndInputModel";
+
+export {default as TanoshiLinkAsButton} from "./link/TanoshiLinkAsButton/TanoshiLinkAsButton.svelte"
+export {default as TanoshiLinkAsButtonModel} from "./link/TanoshiLinkAsButton/TanoshiLinkAsButtonModel";
+
+export {default as TanoshiLinkAsImage} from "./link/TanoshiLinkAsImage/TanoshiLinkAsImage.svelte"
+export {default as TanoshiLinkAsImageModel} from "./link/TanoshiLinkAsImage/TanoshiLinkAsImageModel";
+
+export {default as TanoshiLinkAsNavbarDropdown} from "./link/TanoshiLinkAsNavbarDropdown/TanoshiLinkAsNavbarDropdown.svelte"
+export {default as TanoshiLinkAsNavbarDropdownModel} from "./link/TanoshiLinkAsNavbarDropdown/TanoshiLinkAsNavbarDropdownModel";
+
+export {default as TanoshiModal} from "./modal/TanoshiModal.svelte";
+export {default as TanoshiModalModel} from "./modal/TanoshiModalModel";
+
 export {default as TanoshiNavigation} from "./navigation/TanoshiNavigation.svelte";
 export {default as TanoshiNavigationModel} from "./navigation/TanoshiNavigationModel";
+
+export {default as TanoshiTabContainer} from "./tab/tabContainer/TanoshiTabContainer.svelte";
+export {default as TanoshiTabContainerModel} from "./tab/tabContainer/TanoshiTabContainerModel";
+
+export {default as TanoshiTabContent} from "./tab/tabContent/TanoshiTabContent.svelte";
+
+export {default as TanoshiTabTitle} from "./tab/tabTitle/TanoshiTabTitle.svelte";
+export {default as TanoshiTabTitleModel} from "./tab/tabTitle/TanoshiTabTitleModel";
