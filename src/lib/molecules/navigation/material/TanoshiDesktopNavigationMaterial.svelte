86032af8e22f9a962496460827325da0dccd09c9
Edenn Touitou <deozza@gmail.com>
2023-10-25 10:31:29 +0000

    main vertical is done, need to handle dropdown

--- src/lib/molecules/navigation/material/TanoshiDesktopNavigationMaterial.svelte
+++ src/lib/molecules/navigation/material/TanoshiDesktopNavigationMaterial.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { CONTAINER_ORIENTATIONS, CONTAINER_ITEMS_SPACING, CONTAINER_ITEMS_ALIGNMENTS, WIDTHS} from "$lib/enums";
+	import { CONTAINER_ORIENTATIONS, CONTAINER_ITEMS_SPACING, CONTAINER_ITEMS_ALIGNMENTS, WIDTHS, NAVIGATION_ORIENTATIONS, HEIGHTS} from "$lib/enums";
 	import { TanoshiContainerMaterial, TanoshiContainerModel } from "$molecules";
 	import TanoshiLinkAsNavbarDropdown from '$molecules/link/TanoshiLinkAsNavbarDropdown/TanoshiLinkAsNavbarDropdown.svelte';
 	import type TanoshiDesktopNavigationModel from "../TanoshiDesktopNavigationModel";
@@ -26,13 +26,69 @@
 		.setWidth(WIDTHS.W12)
 		.setDesktopSpacing(CONTAINER_ITEMS_SPACING.Between)
 
+	const tanoshiNavVerticalContainerModel: TanoshiContainerModel = new TanoshiContainerModel(CONTAINER_ORIENTATIONS.C)
+		.setWidth(WIDTHS.W12)
+		.setDesktopSpacing(CONTAINER_ITEMS_SPACING.Start)
+
+	if(tanoshiDesktopNavigationModel.orientation === NAVIGATION_ORIENTATIONS.Vertical){
+		navigationDesktopContainerModel.setHeight(HEIGHTS.H100PRCT)
+	}
+
 </script>
 
-<nav>
-	<TanoshiContainerMaterial tanoshiContainerModel={navigationDesktopContainerModel}>
-		<TanoshiContainerMaterial tanoshiContainerModel={tanoshiNavWidthContainerModel}>
-			{#if tanoshiDesktopNavigationModel.itemsAtLeft.length > 0}
-				<TanoshiContainerMaterial tanoshiContainerModel={desktopNavigationLeftModuleContainer}>
+
+{#if tanoshiDesktopNavigationModel.orientation === NAVIGATION_ORIENTATIONS.Horizontal}
+	<nav class="{tanoshiDesktopNavigationModel.orientation}">
+		<TanoshiContainerMaterial tanoshiContainerModel={navigationDesktopContainerModel}>
+			<TanoshiContainerMaterial tanoshiContainerModel={tanoshiNavWidthContainerModel}>
+				{#if tanoshiDesktopNavigationModel.itemsAtLeft.length > 0}
+					<TanoshiContainerMaterial tanoshiContainerModel={desktopNavigationLeftModuleContainer}>
+						{#each tanoshiDesktopNavigationModel.itemsAtLeft as navbarItemModel }
+							{#if navbarItemModel.component === TanoshiLinkAsNavbarDropdown}
+							<div class="dropdown">
+								<svelte:component this={navbarItemModel.component} tanoshiLinkModel={navbarItemModel.link} />
+							</div>
+							{:else}
+								<svelte:component this={navbarItemModel.component} tanoshiLinkModel={navbarItemModel.link} />
+							{/if}
+						{/each}
+					</TanoshiContainerMaterial>				
+				{/if}
+				{#if tanoshiDesktopNavigationModel.itemsAtCenter.length > 0}
+					<TanoshiContainerMaterial tanoshiContainerModel={desktopNavigationCenterModuleContainer}>
+						{#each tanoshiDesktopNavigationModel.itemsAtCenter as navbarItemModel }
+							{#if navbarItemModel.component === TanoshiLinkAsNavbarDropdown}
+							<div class="dropdown">
+								<svelte:component this={navbarItemModel.component} tanoshiLinkModel={navbarItemModel.link} />
+							</div>
+							{:else}
+								<svelte:component this={navbarItemModel.component} tanoshiLinkModel={navbarItemModel.link} />
+							{/if}
+						{/each}
+					</TanoshiContainerMaterial>
+				{/if}
+				{#if tanoshiDesktopNavigationModel.itemsAtRight.length > 0}
+					<TanoshiContainerMaterial tanoshiContainerModel={desktopNavigationRightModuleContainer}>
+						{#each tanoshiDesktopNavigationModel.itemsAtRight as navbarItemModel }
+							{#if navbarItemModel.component === TanoshiLinkAsNavbarDropdown}
+							<div class="dropdown">
+								<svelte:component this={navbarItemModel.component} tanoshiLinkModel={navbarItemModel.link} />
+							</div>
+							{:else}
+								<svelte:component this={navbarItemModel.component} tanoshiLinkModel={navbarItemModel.link} />
+							{/if}
+						{/each}
+					</TanoshiContainerMaterial>
+				{/if}
+
+			</TanoshiContainerMaterial>
+		</TanoshiContainerMaterial>
+	</nav>
+{:else}
+	<nav class="{tanoshiDesktopNavigationModel.orientation}">
+		<TanoshiContainerMaterial tanoshiContainerModel={navigationDesktopContainerModel}>
+			<TanoshiContainerMaterial tanoshiContainerModel={tanoshiNavVerticalContainerModel}>
+				{#if tanoshiDesktopNavigationModel.itemsAtLeft.length > 0}
 					{#each tanoshiDesktopNavigationModel.itemsAtLeft as navbarItemModel }
 						{#if navbarItemModel.component === TanoshiLinkAsNavbarDropdown}
 						<div class="dropdown">
@@ -42,10 +98,8 @@
 							<svelte:component this={navbarItemModel.component} tanoshiLinkModel={navbarItemModel.link} />
 						{/if}
 					{/each}
-				</TanoshiContainerMaterial>				
-			{/if}
-			{#if tanoshiDesktopNavigationModel.itemsAtCenter.length > 0}
-				<TanoshiContainerMaterial tanoshiContainerModel={desktopNavigationCenterModuleContainer}>
+				{/if}
+				{#if tanoshiDesktopNavigationModel.itemsAtCenter.length > 0}
 					{#each tanoshiDesktopNavigationModel.itemsAtCenter as navbarItemModel }
 						{#if navbarItemModel.component === TanoshiLinkAsNavbarDropdown}
 						<div class="dropdown">
@@ -55,10 +109,8 @@
 							<svelte:component this={navbarItemModel.component} tanoshiLinkModel={navbarItemModel.link} />
 						{/if}
 					{/each}
-				</TanoshiContainerMaterial>
-			{/if}
-			{#if tanoshiDesktopNavigationModel.itemsAtRight.length > 0}
-				<TanoshiContainerMaterial tanoshiContainerModel={desktopNavigationRightModuleContainer}>
+				{/if}
+				{#if tanoshiDesktopNavigationModel.itemsAtRight.length > 0}
 					{#each tanoshiDesktopNavigationModel.itemsAtRight as navbarItemModel }
 						{#if navbarItemModel.component === TanoshiLinkAsNavbarDropdown}
 						<div class="dropdown">
@@ -68,23 +120,35 @@
 							<svelte:component this={navbarItemModel.component} tanoshiLinkModel={navbarItemModel.link} />
 						{/if}
 					{/each}
-				</TanoshiContainerMaterial>
-			{/if}
-
+				{/if}
+			</TanoshiContainerMaterial>
 		</TanoshiContainerMaterial>
-	</TanoshiContainerMaterial>
-</nav>
+
+	</nav>
+{/if}
+
+
 
 <style>
 
 
-nav{
+nav.horizontal{
 	position: fixed;
 	width: 100vw;
 	display: block;
     inset: 0;
 	z-index: 1;
 	max-height: 64px;
+}
+
+nav.vertical {
+	position: fixed;
+	width: 25%;
+	height: 100%;
+	margin: 0;
+	padding: 0;
+	overflow: auto;
+
 }
 
 
